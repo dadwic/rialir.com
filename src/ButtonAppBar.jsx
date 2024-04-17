aaf709cbedf558f71d5713a3287db8a43e2f5591
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-17 04:15:43 +0300

    update title and domain

--- src/ButtonAppBar.jsx
+++ src/ButtonAppBar.jsx
@@ -35,7 +35,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            rialir.com
+            isturkiye.com
           </Typography>
         </Toolbar>
       </AppBar>
